988a2ddf08d8404427cedbac5c04c7e4128d7595
oliviakim04 <165843525+oliviakim04@users.noreply.github.com>
2024-05-16 15:57:34 -0700

    Add files via upload

---
+++ Chart_1.R
@@ -0,0 +1,13 @@
+library("readxl")
+library("ggplot2")
+library("dplyr")
+data <- read_excel("/Users/oliviakim/Downloads/Colorado Mental Health Numerical Survey Results (1).xlsx")
+
+data_filtered <- filter(data, !is.na(data$`How many hours a day do you spend on social media?`))
+
+chart <- ggplot(data_filtered, aes(x = `How many hours a day do you spend on social media?`)) + 
+  geom_histogram(binwidth = 1, color="black", fill = "blue") + 
+  labs(title = "Hours Spent on Social Media Daily", y = "# of People", x = "Hours") +
+  scale_x_continuous(breaks = seq(0, 20, 1))
+
+chart
