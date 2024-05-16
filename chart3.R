20a9a89f55c1ec25810971dc237bc290d8c2f64a
harshita singh <165843455+harshitaSingh121@users.noreply.github.com>
2024-05-16 15:27:06 -0700

    Add files via upload

---
+++ chart3.R
@@ -0,0 +1,21 @@
+
+
+install.packages(readxl)
+install.packages(ggplot2)
+
+library("ggplot2")
+library("readxl")
+
+
+
+file_path <- "/Users/harshitasingh/Downloads/Colorado Mental Health Numerical Survey Results.xlsx"
+
+data <- read_excel(file_path)
+
+response_summary <- table(data$'Do you try to limit your social media use?')
+
+labels <- c("No", "Yes")
+
+pie(response_summary, labels = labels, main = "Social Media Usage Limitation", col = c("red", "green"))
+
+legend("topleft", legend = labels, fill = c("red", "green"), title = "Response")
