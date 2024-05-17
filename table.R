af1dec591730a01119a452ab86be80fc9fcd9c5d
cw2208 <165843488+cw2208@users.noreply.github.com>
2024-05-16 17:38:15 -0700

    Update table.R

--- table.R
+++ table.R
@@ -2,7 +2,6 @@ library("readxl")
 library("dplyr")
 data <- read_excel("/Users/cwang/Downloads/Colorado Mental Health Numerical Survey Results (1).xlsx")
 
-daily_hours_spent <- data$`How many hours a day do you spend on social media?`
 table <- data %>%
   group_by(`Have you been diagnosed with a mental disorder?`) %>%
   select(`How many hours a day do you spend on social media?`, `How many hours a week do you spend on social media?`,
