b488b06ff4fddae1bd9bd27c0cde90d8d4d1a2b9
chaewonyoo37 <165843717+chaewonyoo37@users.noreply.github.com>
2024-05-16 19:33:44 -0700

    Update chart 2.R
    update

--- chart 2.R
+++ chart 2.R
@@ -1,7 +1,7 @@
 library("readxl")
 library("ggplot2")
 
-data <- read_excel("C:/Users/chaew/Downloads/Colorado Mental Health Numerical Survey Results.xlsx")
+data <- read_excel("Colorado Mental Health Numerical Survey Results.xlsx")
 
 ggplot(data, aes(x = `Emotional Wellbeing Total`, y = `How many hours a week do you spend on social media?`)) +
   geom_point(size = 2, shape = 23, color = "blue") +
